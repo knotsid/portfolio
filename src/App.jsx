3d1f7ecbedfb88cec54631b66d2cb577ab3d9b34
Not Sid <kumarsid6500@gmail.com>
2023-05-04 02:27:51 +0530

    added home page (fully responsive)

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import "./App.css";
 
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
 	return (
 		<>
 			<Header />
+			<main className="main">
+				<Home />
+			</main>
 		</>
 	);
 }
