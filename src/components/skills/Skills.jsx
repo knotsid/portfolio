5b5188b93402258acb6977fee3af807515b6d7cd
Not Sid <kumarsid6500@gmail.com>
2023-05-11 23:21:22 +0530

    added Services section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -9,10 +9,10 @@ const Skills = () => {
 			<h2 className="section__title">Skills</h2>
 			<span className="section__subtitle">My technical level</span>
 
-               <div className="skills__container container grid">
-                    <Frontend />
-                    <Backend />
-               </div>
+			<div className="skills__container container grid">
+				<Frontend />
+				<Backend />
+			</div>
 		</section>
 	);
 };
