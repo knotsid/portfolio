d9c0c542c8ab341e73f001fc9202b20db95e29ca
Not Sid <kumarsid6500@gmail.com>
2023-07-01 22:32:53 +0530

    added contact us section with email sumbmission

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
+import Contact from "./components/contact/Contact";
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 				<About />
 				<Skills />
 				<Services />
+				<Contact />
 			</main>
 		</>
 	);
