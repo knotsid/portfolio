657e2c03324906131824125b54141f50bcbba53b
Not Sid <kumarsid6500@gmail.com>
2023-07-10 01:40:28 +0530

    added footer and scroll up feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scrollup/ScrollUp";
 
 function App() {
 	return (
@@ -19,6 +21,8 @@ function App() {
 				<Services />
 				<Contact />
 			</main>
+			<Footer />
+			<ScrollUp />
 		</>
 	);
 }
